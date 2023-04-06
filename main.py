5c31af3137972ea7af1a6ab252c9cae6ada37d1b
Allen Tao <taoqingqiu@gmail.com>
2023-04-06 23:50:48 +0800

    build: ready for integrating with frontend

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from functools import reduce
 from pathlib import Path
 
 from apiflask import APIFlask
+
 from models.schemas.response import BaseResponse
 from utils.repository import use_db, init_redis_conn_pool
 from utils.common import get_conf
@@ -13,7 +14,7 @@ load_dotenv(verbose=True)
 
 def create_app():
     _app = APIFlask(__name__)
-    _app.config['SQLALCHEMY_DATABASE_URI'] = get_conf().get('db').get('sqlite_url')
+    _app.config['SQLALCHEMY_DATABASE_URI'] = get_conf().get('db').get('db_url')
     _app.config["BASE_RESPONSE_SCHEMA"] = BaseResponse
 
     redis_url = get_conf().get('db').get('redis_url')
@@ -25,14 +26,10 @@ def create_app():
         # register views & models
         vms = reduce(lambda pre, curr: not pre.extend(curr) and pre, [
             [f'{pkg}.{p.stem}' for p in Path(pkg).glob('[!_]*.py')] for pkg in ['models', 'views']], [])
-
         for vm in vms:
             importlib.import_module(vm)
 
         _db.create_all()
-
-        from services.user import create_admin
-        create_admin()
 
         from utils.permissions import bind_authentication_checker
         bind_authentication_checker(_app)
@@ -41,3 +38,7 @@ def create_app():
 
 
 app = create_app()
+
+if __name__ == '__main':
+    from services.user import create_admin
+    create_admin()
